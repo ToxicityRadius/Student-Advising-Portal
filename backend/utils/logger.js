33aec9d201449d698df8c4a7c5980f0a75a8533b
mcadelacruz <mcadelacruz@tip.edu.ph>
2026-03-22 13:17:29 +0800

    backend loading fixes

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,17 +1,19 @@
 const pino = require('pino');
+const pinoPretty = require('pino-pretty');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const logger = pino({
-  level: process.env.LOG_LEVEL || (isProduction ? 'info' : 'debug'),
-  ...(isProduction
-    ? {}
-    : {
-        transport: {
-          target: 'pino/file',
-          options: { destination: 1 } // stdout
-        }
+const logger = pino(
+  {
+    level: process.env.LOG_LEVEL || (isProduction ? 'info' : 'debug')
+  },
+  isProduction
+    ? undefined
+    : pinoPretty({
+        colorize: true,
+        translateTime: 'SYS:standard',
+        ignore: 'pid,hostname'
       })
-});
+);
 
 module.exports = logger;
